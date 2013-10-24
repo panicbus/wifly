77a07df87e2a3c7ffc9bef054ffa3862c4b48de7
Nico Crisafulli <elementalair@gmail.com>
2013-10-24 15:05:02 -0700

    minor updates to style and flow

--- app/helpers/airports_helper.rb
+++ app/helpers/airports_helper.rb
@@ -1,10 +1,8 @@
 module AirportsHelper
   def create_location(airport_code)
-     # query = airport_code.gsub(/\s/,'%20')
-        # encrypt key please
+        # encrypt key please!!
     airport = Airport.find_by_code(airport_code)
     result = Typhoeus.get("http://api.locu.com/v1_0/venue/search/?name=#{airport_code}&api_key=2bec4d817e50110d2223c53e5aea35a06503cf1a")
-
     # parses the json from googlemaps API query
     result_hash = JSON.parse(result.body)
     #iterates through the array of hashes and grabs lat & lng
@@ -12,6 +10,7 @@ module AirportsHelper
         @lat = result['lat']
         @long = result['long']
       end
+      # makes new instance of Location object in db with these keys
     Location.create(longitude: @long, latitude: @lat, airport_id: airport.id)
   end
 end
